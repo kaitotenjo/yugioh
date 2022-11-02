a13bee2862c2fb7b229484584a2974d2444e14e8
kaito tenjo <toiyeuranmori@gmail.com>
2022-11-02 17:11:15 +0700

    upload image user

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
-  devise_for :users ,:controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }  
+  devise_for :users , controllers: { 
+    omniauth_callbacks: "users/omniauth_callbacks" ,
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get 'u/:id', to: 'users#show', as:'user'  
   resources :homes
   resources :product
   resources :users
