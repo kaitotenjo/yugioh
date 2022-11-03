c893813f78cc80fb9900019613487a1b53102790
kaito tenjo <toiyeuranmori@gmail.com>
2022-11-03 11:05:15 +0700

    product detail

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,21 @@
-class ProductController < ApplicationController
-  before_action :get_product , only: [:index]
+class ProductsController < ApplicationController
+  before_action :get_product , only: [:index,:show]
+  before_action :load_user, only: [:index, :show ]
 
   def index
   end
 
   def show 
+    @card_sets.each do |card_set|
+      if card_set.has_value?(params[:id].to_s)
+        @card_detail=card_set
+      else
+        
+      end
+   
+    end
   end
+  
 
   private
   
@@ -24,6 +34,14 @@ class ProductController < ApplicationController
     end
     @card_sets=@products["card_sets"]
     @images = @products["card_images"]
+  end
+
+  def load_user
+    if user_signed_in?
+    @user = User.find_by(id: current_user.id)
+    else 
+        
+    end
   end
 
 end
