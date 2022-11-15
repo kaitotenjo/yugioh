cd7735206644efdc285067726dcb2ae2bef8a8e6
kaito tenjo <toiyeuranmori@gmail.com>
2022-11-15 14:59:55 +0700

    cart in menu

--- app/javascript/cart.js
+++ app/javascript/cart.js
@@ -1,15 +1,8 @@
-$(document).ready(function() {
-    $("#quantity").on("click",function() {
-        $.ajax({
-            url:"/cart/update_quantity",
-            method: "post",
-            data: {"quantity_id":$(this).val()} ,
-            dataType: "json",
-            success: function(data) {
-                alert('successfully');
-              }
-            
-        }) 
-      
+
+(function(){
+ 
+    $("#cart").on("click", function() {
+      $(".shopping-cart").fadeToggle( "fast");
     });
-});
+    
+  })();
